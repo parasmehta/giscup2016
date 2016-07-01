944083cdfce8284be41e98c2b1dbfad79c7c4728
Christian Windolf <christianwindolf@web.de>
2016-07-01 16:17:07 +0200

    create test for cell determination

---
+++ src/test/scala/edu/fuberlin/hotspots/CellDeterminationSpec.scala
@@ -0,0 +1,25 @@
+package edu.fuberlin.hotspots
+
+import com.esri.core.geometry.Point
+import org.joda.time.DateTime
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+/**
+  * Created by Christian Windolf on 29.06.16.
+  */
+class CellDeterminationSpec extends FlatSpec with Matchers with BeforeAndAfter {
+  val location:STPoint = new STPoint(new Point(-73.956960, 40.794516), new DateTime(2015, 1, 30, 18, 59))
+  val cellOf = cellsFor(0.001, 1)
+
+  it should "have the correct long cell id" in {
+    cellOf(location)._1 shouldBe -73956
+  }
+
+  it should "have the correct lat cell id" in {
+    cellOf(location)._2 shouldBe 40794
+  }
+
+  it should "have the correct time cell" in {
+    cellOf(location)._3 shouldBe 30
+  }
+}
