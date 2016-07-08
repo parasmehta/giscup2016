554057312b0c26cbf0a349a6b7a6fb9d97c05b16
Christian Windolf <christianwindolf@web.de>
2016-07-08 11:32:07 +0200

    create second test for plain data

---
+++ src/test/scala/edu/fuberlin/hotspots/GetisOrdWithoutHotspotsSpec.scala
@@ -0,0 +1,29 @@
+package edu.fuberlin.hotspots
+
+import org.scalatest.Matchers
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by Christian Windolf on 08.07.16.
+  */
+class GetisOrdWithoutHotspotsSpec extends SparkSpec with Matchers {
+  def createTestData():Array[((Long, Long, Long), Int)] = {
+    val data = new ListBuffer[((Long, Long, Long), Int)]()
+    for(x <- 1 to 100; y <- 1 to 100; t <- 1 to 100) {
+      data.append(((x, y , t), 1))
+    }
+    data.toArray
+  }
+
+  it should "not return more items than the input" in { f =>
+    GetisOrd.calculate(f.context.parallelize(createTestData)).count shouldBe <=(10000)
+  }
+
+  it should "give every cell the same value" in { f =>
+    val results = GetisOrd.calculate(f.context.parallelize(createTestData)).collect.toMap
+    for(x <- 2 to 9; y <- 2 to 9; t <- 2 to 9){
+      results((x, y, t)) shouldEqual 1.0 +- 0.1
+    }
+  }
+}
