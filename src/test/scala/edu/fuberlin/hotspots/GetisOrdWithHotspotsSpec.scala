344e70f068fcf6ee68664a8c3b85e67e2a363b2b
Christian Windolf <christianwindolf@web.de>
2016-07-08 11:31:49 +0200

    code cleanup in tests

--- src/test/scala/edu/fuberlin/hotspots/GetisOrdWithHotspotsSpec.scala
+++ src/test/scala/edu/fuberlin/hotspots/GetisOrdWithHotspotsSpec.scala
@@ -2,9 +2,7 @@ package edu.fuberlin.hotspots
 
 import java.lang.Math.{pow, sqrt}
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.{BeforeAndAfter, Matchers}
+import org.scalatest.Matchers
 
 import scala.collection.mutable.ListBuffer
 
