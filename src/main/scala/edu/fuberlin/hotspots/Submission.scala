f53a982641d1eabfb409cce118808dbf15786793
Christian Windolf <christianwindolf@web.de>
2016-08-03 16:01:45 +0200

    use top function instead of sort().take()

--- src/main/scala/edu/fuberlin/hotspots/Submission.scala
+++ src/main/scala/edu/fuberlin/hotspots/Submission.scala
@@ -1,9 +1,11 @@
 package edu.fuberlin.hotspots
 
+import java.io.{BufferedOutputStream, File, FileOutputStream}
 import java.math.BigDecimal
 
 import org.apache.spark.{SparkConf, SparkContext}
 import SparkHelpers._
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 /**
   * Created by Christian Windolf on 29.06.16.
@@ -43,10 +45,23 @@ object Submission {
     conf.registerKryoClasses(Array(classOf[Trip], classOf[SuperCell], classOf[SuperCellFactory],
       classOf[Option[Trip]], classOf[(Cellid, Int)], classOf[(Cellid, Double)],
       classOf[Array[Double]], classOf[org.apache.spark.util.StatCounter],
-      classOf[scala.reflect.ClassTag$$anon$1], classOf[java.lang.Class[_]]
+      classOf[scala.reflect.ClassTag$$anon$1], classOf[java.lang.Class[_]],
+      ordering.getClass, classOf[scala.collection.mutable.WrappedArray$ofRef], classOf[Array[String]]
     ))
     val sc = new SparkContext(conf)
     submit(sc, inputDirectory, outputFile, gridSize, timeSpan, sample)
+  }
+
+  implicit val ordering = new Ordering[(Cellid, Double, Double)]{
+    override def compare(a:(Cellid, Double, Double), b:(Cellid, Double, Double)):Int = {
+      if(a._2 == b._2){
+        return 0
+      } else if(a._2 < b._2){
+        return -1
+      } else{
+        return 1
+      }
+    }
   }
 
   def printHelp = {
@@ -61,13 +76,22 @@ object Submission {
 
   def submit(sc:SparkContext,
              inputDir:String,
-             outputDir:String,
+             outputFile:String,
              gridSize:Any,
              timeSpan:Any,
              sample:Double):Unit = {
     val taxiData = sc.loadTaxi(inputDir, sample)
     val zvalues = GetisOrd.calculate(taxiData, gridSize, timeSpan)
-    val output = zvalues.sortBy(_._2, false).map(c=> s"${c._1._1}, ${c._1._2}, ${c._1._3}, ${c._2}, ${c._3}")
-    output.saveAsTextFile(outputDir)
+    val output = zvalues.top(50).map(c=> s"${c._1._1}, ${c._1._2}, ${c._1._3}, ${c._2}, ${c._3}")
+    val stream = outputFile.slice(0,4).toLowerCase match {
+      case "hdfs" => {
+        val fs = FileSystem.get(sc.hadoopConfiguration)
+        fs.create(new Path(outputFile))
+      }
+      case _ => new FileOutputStream(new File(outputFile))
+    }
+    output.foreach(line => stream.write((line + "\n").getBytes()))
+    stream.flush()
+    stream.close()
   }
 }
